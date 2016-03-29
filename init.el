ac3e90e39691c6fc074dea9f6be246eb4155cfa5
Li Xinyang <lixinyang1026@gmail.com>
2016-03-29 16:53:35 +0800

    Update init file

--- init.el
+++ init.el
@@ -163,6 +163,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 1)
  '(package-selected-packages (quote (company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
