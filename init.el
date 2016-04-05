0f7bcd24c57ed38e19d1dec5125ec568113ec9ab
Li Xinyang <lixinyang1026@gmail.com>
2016-04-05 16:51:08 +0800

    Add org-mode keybinding more

--- init.el
+++ init.el
@@ -202,7 +202,10 @@
 ;; Org-mode Related Key Binding
 ;; -------------------------------------------------------------------
 ;; Org mode agenda vide default key binding
+(global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
 
 
 ;;  __   __   __            __   __   ___
